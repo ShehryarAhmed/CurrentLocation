7c8e9d1e0f52852ed850fef0d8fd3ace37dab6c2
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-07-01 00:42:58 +0500

    working lesson 3

--- GoogleFusedLocationProvider/app/src/main/java/com/example/android/googlefusedlocationprovider/MainActivity.java
+++ GoogleFusedLocationProvider/app/src/main/java/com/example/android/googlefusedlocationprovider/MainActivity.java
@@ -1,37 +1,58 @@
 package com.example.android.googlefusedlocationprovider;
 
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener
+        com.google.android.gms.location.LocationListener
 {
 
+    private String LOG_TAG = "MainActivity";
     private TextView tvLcation;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private Location mLastLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
+      /*  mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
-                .build();
-
+                .build();*/
+        buildGoogleApiClient();
         tvLcation = (TextView) findViewById(R.id.tvlocation);
     }
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
 
     @Override
     protected void onStart() {
@@ -49,12 +70,39 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onConnected(Bundle bundle) {
-        mLocationRequest = LocationRequest.create();
+       /* mLocationRequest = LocationRequest.create();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         mLocationRequest.setInterval(1000);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+*/
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;}
+       mLastLocation =  LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,mLocationRequest,this);
-        
+       tvLcation.setText("Latitude : "+mLastLocation.getLatitude()
+               +"\n Longitude : " +mLastLocation.getLongitude()
+               +"\n Speed : "+mLastLocation.getSpeed()
+               + "\n Services Proovider : "+ mLastLocation.getProvider() );
+
 
     }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(LOG_TAG, "GoogleApiClient Suspended");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(LOG_TAG, "GoogleApiClient connection has failed");
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        tvLcation.setText(""+location.getLatitude());
+        }
 }
